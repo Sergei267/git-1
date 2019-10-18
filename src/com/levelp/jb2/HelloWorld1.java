d25d209fb0910a8d8dfba89da4e8c280fce543d6
sele0919 <sergei.lebedev@netcracker.com>
2019-10-18 14:42:07 +0400

    Two java-files

---
+++ src/com/levelp/jb2/HelloWorld1.java
@@ -0,0 +1,8 @@
+package com.levelp.jb2;
+
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Wolrd");
+
+    }
+}
---
+++ src/com/levelp/jb2/HelloWorld2.java
@@ -0,0 +1,8 @@
+package com.levelp.jb2;
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Wolrd 2");
+
+    }
+}
